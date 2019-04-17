c4a6d4457cdfdda83a0da2981f91ea45b3559c4e
karolisli <karolisliesys@gmail.com>
2019-04-17 17:00:37 +0300

    uzduotis#3

--- INdex.php
+++ INdex.php
@@ -1,17 +1,20 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+    <?php 
+    $sudas1 = (rand(1,100)) ;
+    $sudas2 = (rand(101,200)) ;
+    $sudas3 = (rand(201,300)) ;
+    $sudas4 = (rand(301,400)) ;
+  
+    ?>
+
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Komentarai</title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <h1>Skolos skaiciuokle <?php echo $sudas1;?></h1>
+        <h2>Jei paemei jievru <?php echo $sudas2;?></h2>
+        <h3>Su dviem kabanciais grazinsi <?php echo $sudas3;?></h3>
+        <h4>Su vienu kabanciu grazinsi <?php echo $sudas4;?></h4>
     </body>
-</html>
+</html> 
